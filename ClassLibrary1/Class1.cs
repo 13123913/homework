f85369b3ac6afccdb6e96a7e4ef8d319b5f89662
13123913 <perelygin_0505@mail.ru>
2023-04-19 21:42:50 +0700

    Based

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFunc
+{
+    public class Class1
+    {
+        public static void SumFunc(double x, double y)
+        {
+            double Cul = x * y + 2 * x;
+            double Sum = x + y;
+            double Mult = x * y;
+            Console.WriteLine("Amount = {0} Multiply = {1} Cult = {2}", Sum, Mult, Cul);
+            Console.ReadLine();
+        }
+    }
+}
